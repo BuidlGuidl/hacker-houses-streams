60bb7eb0fe4f1f2c14f61930ec25a22b6ca03911
Austin Griffith <austin@ethereum.org>
2023-03-23 10:11:34 -0600

    added a new contract and a little frontend

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -1,78 +1,64 @@
-//SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
+//SPDX-License-Identifier: MIT
 
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
-/**
- * A smart contract that allows changing a state variable of the contract and tracking the changes
- * It also allows the owner to withdraw the Ether in the contract
- * @author BuidlGuidl
- */
-contract YourContract {
 
-    // State Variables
-    address public immutable owner;
-    string public greeting = "Building Unstoppable Apps!!!";
-    bool public premium = false;
-    uint256 public totalCounter = 0;
-    mapping(address => uint) public userGreetingCounter;
+contract YourContract is Ownable {
 
-    // Events: a way to emit log statements from smart contract that can be listened to by external parties
-    event GreetingChange(address greetingSetter, string newGreeting, bool premium, uint256 value);
+    struct BuilderStreamInfo {
+        uint256 cap;
+        uint256 last;
+    }
+    mapping(address => BuilderStreamInfo) public streamedBuilders;
 
-    // Constructor: Called once on contract deployment
-    // Check packages/hardhat/deploy/00_deploy_your_contract.ts
-    constructor(address _owner) {
-        owner = _owner;
+    uint256 public frequency = 300 seconds; //2592000; // 30 days
+
+    event Withdraw(address indexed to, uint256 amount, string reason);
+
+    constructor(address startingOwner) {
+      _transferOwnership(startingOwner);
     }
 
-    // Modifier: used to define a set of rules that must be met before or after a function is executed
-    // Check the withdraw() function
-    modifier isOwner() {
-        // msg.sender: predefined variable that represents address of the account that called the current function
-        require(msg.sender == owner, "Not the Owner");
-        _;
-    }
+    function unlockedBuilderAmount(address _builder) public view returns (uint256) {
+        BuilderStreamInfo memory builderStream = streamedBuilders[_builder];
+        require(builderStream.cap > 0, "No active stream for builder");
 
-    /**
-     * Function that allows anyone to change the state variable "greeting" of the contract and increase the counters
-     *
-     * @param _newGreeting (string memory) - new greeting to save on the contract
-     */
-    function setGreeting(string memory _newGreeting) public payable {
-        // Print data to the hardhat chain console. Remove when deploying to a live network.
-        console.log("Setting new greeting '%s' from %s",  _newGreeting, msg.sender);
-
-        // Change state variables
-        greeting = _newGreeting;
-        totalCounter += 1;
-        userGreetingCounter[msg.sender] += 1;
-
-        // msg.value: built-in global variable that represents the amount of ether sent with the transaction
-        if (msg.value > 0) {
-            premium = true;
-        } else {
-            premium = false;
+        if (block.timestamp - builderStream.last > frequency) {
+            return builderStream.cap;
         }
 
-        // emit: keyword used to trigger an event
-        emit GreetingChange(msg.sender, _newGreeting, msg.value > 0, 0);
+        return (builderStream.cap * (block.timestamp - builderStream.last)) / frequency;
     }
 
-    /**
-     * Function that allows the owner to withdraw all the Ether in the contract
-     * The function can only be called by the owner of the contract as defined by the isOwner modifier
-     */
-    function withdraw() isOwner public {
-        (bool success,) = owner.call{value: address(this).balance}("");
-        require(success, "Failed to send Ether");
+    function addBuilderStream(address payable _builder, uint256 _cap) public onlyOwner {
+        streamedBuilders[_builder] = BuilderStreamInfo(_cap, block.timestamp - frequency);
+        // probably need an event here 
     }
 
-    /**
-     * Function that allows the contract to receive ETH
-     */
+    function streamWithdraw(uint256 _amount, string memory _reason) public {
+        require(address(this).balance >= _amount, "Not enough funds in the contract");
+        BuilderStreamInfo storage builderStream = streamedBuilders[msg.sender];
+        require(builderStream.cap > 0, "No active stream for builder");
+
+        uint256 totalAmountCanWithdraw = unlockedBuilderAmount(msg.sender);
+        require(totalAmountCanWithdraw >= _amount,"Not enough in the stream");
+
+        uint256 cappedLast = block.timestamp - frequency;
+        if (builderStream.last < cappedLast){
+            builderStream.last = cappedLast;
+        }
+
+        builderStream.last = builderStream.last + ((block.timestamp - builderStream.last) * _amount / totalAmountCanWithdraw);
+
+        (bool sent,) = msg.sender.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
+
+        emit Withdraw(msg.sender, _amount, _reason);
+    }
+
+    // to support receiving ETH by default
     receive() external payable {}
+    fallback() external payable {}
 }
