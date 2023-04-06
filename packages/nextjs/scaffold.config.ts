cb6a4c9351439505db63c113af2e40d0fa5e2175
Austin Griffith <austin@ethereum.org>
2023-04-06 16:11:25 -0600

    deploy to mainnet

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -8,7 +8,7 @@ type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The network where your DApp lives in
-  targetNetwork: chains.hardhat,
+  targetNetwork: chains.mainnet,
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect on the local network
