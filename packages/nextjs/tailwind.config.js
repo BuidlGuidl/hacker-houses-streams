963a0670fa351a2b5affc24637e76601403df89b
Pablo Alayeto <55535804+Pabl0cks@users.noreply.github.com>
2023-06-07 11:11:28 +0200

    Dark mode (#16)

--- packages/nextjs/tailwind.config.js
+++ packages/nextjs/tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./utils/**/*.{js,ts,jsx,tsx}"],
   plugins: [require("daisyui")],
-  darkTheme: "scaffoldEth",
+  darkTheme: "scaffoldEthDark",
   // DaisyUI theme colors
   daisyui: {
     themes: [
@@ -32,6 +32,33 @@ module.exports = {
           },
         },
       },
+      {
+        scaffoldEthDark: {
+          primary: "#212638",
+          "primary-content": "#F9FBFF",
+          secondary: "#2A3655",
+          "secondary-content": "#F9FBFF",
+          accent: "#4969A6",
+          "accent-content": "#F9FBFF",
+          neutral: "#F9FBFF",
+          "neutral-content": "#385183",
+          "base-100": "#385183",
+          "base-200": "#2A3655",
+          "base-300": "#212638",
+          "base-content": "#F9FBFF",
+          info: "#385183",
+          success: "#34EEB6",
+          warning: "#FFCF72",
+          error: "#FF8863",
+
+          "--rounded-btn": "9999rem",
+
+          ".tooltip": {
+            "--tooltip-tail": "6px",
+            "--tooltip-color": "hsl(var(--p))",
+          },
+        },
+      }
     ],
   },
   theme: {
